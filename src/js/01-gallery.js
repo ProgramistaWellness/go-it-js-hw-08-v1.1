beab4c3eeeac21bdb7a589b0e8085628fa5c17b2
Oskar Przybysz <117311648+ProgramistaWellness@users.noreply.github.com>
2023-05-23 13:16:53 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import { galleryItems } from './gallery-items.js';
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<li>
+      <a class="gallery__item" href="${image.original}">
+        <img
+          class="gallery__image"
+          src="${image.preview}"
+          alt="${image.description}"
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
+console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', 
+{ captionData: "alt",
+  captionsPosition: "outside",
+  captionDelay: 250,
+});
 console.log(galleryItems);
