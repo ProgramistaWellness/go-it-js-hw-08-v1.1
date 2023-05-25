1fda99d3ef1a248e0e47ba57214a48086f35a411
Oskar Przybysz <117311648+ProgramistaWellness@users.noreply.github.com>
2023-05-25 23:28:33 +0200

    instaled vimeo-player and lodash and finished 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const iframe = document.querySelector('iframe');
+  try {
+    const player = new Player(iframe);
+    const throttled = throttle(function(data) {
+        localStorage.setItem('videoplayer-current-time', data.seconds)
+        }, 1000);
+    player.on('timeupdate', throttled);
+
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(seconds) {
+    }).catch(function(error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+});
